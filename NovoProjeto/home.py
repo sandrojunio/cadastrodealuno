dea66dc5f7d870cd75cd196d131bbd884d5824e4
João Júnior <joaojunior1205@gmail.com>
2021-04-17 12:48:49 -0300

    Organização do código

--- NovoProjeto/home.py
+++ NovoProjeto/home.py
@@ -1,4 +1,4 @@
-from PyQt5 import uic,QtWidgets
+from PyQt5 import uic, QtWidgets
 
 def menu():
     Home.pushButton.clicked.connect(aluno)
@@ -6,25 +6,25 @@ def menu():
     Home.pushButton_3.clicked.connect(atividade)
     Home.pushButton_4.clicked.connect(desempenho)
     Home.pushButton_5.clicked.connect(consulta)
-    
+
     cadastrodealuno.pushButton.clicked.connect(aluno)
     cadastrodealuno.pushButton_2.clicked.connect(assunto)
     cadastrodealuno.pushButton_3.clicked.connect(atividade)
     cadastrodealuno.pushButton_4.clicked.connect(desempenho)
     cadastrodealuno.pushButton_5.clicked.connect(consulta)
-    
+
     cadastrodeassunto.pushButton.clicked.connect(aluno)
     cadastrodeassunto.pushButton_2.clicked.connect(assunto)
     cadastrodeassunto.pushButton_3.clicked.connect(atividade)
     cadastrodeassunto.pushButton_4.clicked.connect(desempenho)
     cadastrodeassunto.pushButton_5.clicked.connect(consulta)
-    
+
     cadastrodeatividade.pushButton.clicked.connect(aluno)
     cadastrodeatividade.pushButton_2.clicked.connect(assunto)
     cadastrodeatividade.pushButton_3.clicked.connect(atividade)
     cadastrodeatividade.pushButton_4.clicked.connect(desempenho)
     cadastrodeatividade.pushButton_5.clicked.connect(consulta)
-    
+
     cadastrodeconsulta.pushButton.clicked.connect(aluno)
     cadastrodeconsulta.pushButton_2.clicked.connect(assunto)
     cadastrodeconsulta.pushButton_3.clicked.connect(atividade)
@@ -38,8 +38,7 @@ def aluno():
     cadastrodeassunto.close()
     cadastrodeatividade.close()
     cadastrodeconsulta.close()
-    
-   
+
 def assunto():
     menu()
     cadastrodeassunto.show()
@@ -47,8 +46,7 @@ def assunto():
     cadastrodealuno.close()
     cadastrodeatividade.close()
     cadastrodeconsulta.close()
-    
-    
+
 def atividade():
     menu()
     cadastrodeatividade.show()
@@ -56,8 +54,7 @@ def atividade():
     cadastrodealuno.close()
     cadastrodeassunto.close()
     cadastrodeconsulta.close()
-    
-    
+
 def desempenho():
     menu()
     cadastrodeassunto.show()
@@ -65,27 +62,22 @@ def desempenho():
     cadastrodealuno.close()
     cadastrodeassunto.close()
     cadastrodeconsulta.close()
-    
-    
+
 def consulta():
     menu()
     cadastrodeconsulta.show()
     Home.close()
     cadastrodealuno.close()
     cadastrodeassunto.close()
-    
-    
-    
-     
-app=QtWidgets.QApplication([])
-Home=uic.loadUi("Home.ui")
-cadastrodealuno=uic.loadUi("cadastrodealuno.ui")
-cadastrodeassunto=uic.loadUi("cadastrodeassunto.ui")
-cadastrodeconsulta=uic.loadUi("cadastrodeconsulta.ui")
-cadastrodeatividade=uic.loadUi("cadastrodeatividade.ui")
+
+app = QtWidgets.QApplication([])
+Home = uic.loadUi("Home.ui")
+cadastrodealuno = uic.loadUi("cadastrodealuno.ui")
+cadastrodeassunto = uic.loadUi("cadastrodeassunto.ui")
+cadastrodeconsulta = uic.loadUi("cadastrodeconsulta.ui")
+cadastrodeatividade = uic.loadUi("cadastrodeatividade.ui")
 
 menu()
-
 
 Home.show()
 app.exec()
